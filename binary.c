1828bddedd3f085460e858991e32a136a1cb6c16
root <root@localhost.localdomain>
2023-04-05 23:30:27 +0100

    correction

--- binary.c
+++ binary.c
@@ -6,20 +6,21 @@
  */
 int binary(va_list *v)
 {
-	unsigned int d = va_arg(*v, unsigned int);
+	unsigned int *d = va_arg(*v, unsigned int *);
 	unsigned int mult_ten = 1;
 	unsigned int rest, total;
 	unsigned int i2, div2 = 1, rest2 = 0;
 	int n = 0;
 
-	while (d != 0)
+	while (*d != 0)
 	{
-		rest = d % 2;
-		d /=  2;
+		rest = *d % 2;
+		*d /=  2;
 		total += mult_ten * rest;
 		mult_ten *= 10;
 	}
-	i2 = total;
+	*d = total;
+	i2 = *d;
 	while (rest2 != i2)
 	{
 		div2 *= 10;
---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned int a = 98;
+    _printf("%b\n", &a);
+	_printf("%d\n", &a);
+    return (0);
+}
