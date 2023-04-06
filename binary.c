0473ee5af93c86754f89112226b5a47b0eb11d87
root <root@localhost.localdomain>
2023-04-06 01:14:34 +0100

    correction

--- binary.c
+++ binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * binary - print the binary counterpart of an unsigned integer
  * @v: a va_list pointer
@@ -6,35 +7,29 @@
  */
 int binary(va_list *v)
 {
-	unsigned int *d = va_arg(*v, unsigned int *);
-	unsigned int mult_ten = 1;
-	unsigned int rest, total;
-	unsigned int i2, div2 = 1, rest2 = 0;
+	link_l *new, *head = NULL;
+	int d = va_arg(*v, int);
+	int rest;
 	int n = 0;
 
-	while (*d != 0)
+	while (d != 0)
 	{
-		rest = *d % 2;
-		*d /=  2;
-		total += mult_ten * rest;
-		mult_ten *= 10;
+		rest = d % 2;
+		d /=  2;
+		new = malloc(sizeof(link_l));
+		new->n = rest;
+		new->next = head;
+		head = new;
 	}
-	*d = total;
-	i2 = *d;
-	while (rest2 != i2)
+
+	while (head != NULL)
 	{
-		div2 *= 10;
-		rest2 = i2 % div2;
-	}
-	for (div2 /= 10; div2 >= 10; div2 /= 10)
-	{
-		rest2 = (i2 - (i2 % div2)) / div2;
-		_putchar('0' + rest2);
+		_putchar('0' + head->n);
 		n++;
-		i2 = i2 % div2;
+		new = head;
+		head = head->next;
+		free(new);
 	}
-	_putchar('0' + i2);
-	n++;
 	return (n);
 }
 
--- main.h
+++ main.h
@@ -12,6 +12,16 @@ typedef struct id
 	char id;
 	int (*func)(va_list *);
 } id_func;
+/**
+ * struct linked - linked list
+ * @n: the data
+ * @next: pointer to the next
+ */
+typedef struct linked
+{
+	int n;
+	struct linked *next;
+} link_l;
 int _putchar(char c);
 int handler(char c, va_list *ppa);
 int _printf(const char *format, ...);
